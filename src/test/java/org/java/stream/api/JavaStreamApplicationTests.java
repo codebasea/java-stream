a69cc3200ff82e45da0a47843f78adb8a0120681
codebasea <110478608+codebasea@users.noreply.github.com>
2022-11-29 23:28:32 +0530

    java stream project

---
+++ src/test/java/org/java/stream/api/JavaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.stream.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
