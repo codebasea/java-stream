cd38bcf7c9985a02b10666791d1311d6defe1daa
codebasea <110478608+codebasea@users.noreply.github.com>
2022-11-29 23:48:31 +0530

    SingleTone class added

---
+++ src/main/java/org/java/stream/api/core/singleton/LazySingleton.java
@@ -0,0 +1,33 @@
+package org.java.stream.api.core.singleton;
+
+
+import java.io.Serializable;
+
+public class LazySingleton extends MyClone implements Serializable {
+
+    private static LazySingleton instance;
+
+    private LazySingleton() {
+        if (instance != null) {
+            throw new IllegalStateException("object can't be create using reflection");
+        }
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+
+    protected Object readResolve() {
+        return instance;
+    }
+
+    public static synchronized LazySingleton getInstance() {
+        if (instance == null) {
+            return instance = new LazySingleton();
+        } else {
+            return instance;
+        }
+    }
+
+}
---
+++ src/main/java/org/java/stream/api/core/singleton/MyClone.java
@@ -0,0 +1,10 @@
+package org.java.stream.api.core.singleton;
+
+
+public class MyClone implements Cloneable {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+}
